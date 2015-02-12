0ae07bb335ce9523d2bfa156292c5cc414b68d24
ochrist <oliver.christ@mediaman.de>
2015-02-12 20:46:20 +0100

    first final

--- index.js
+++ index.js
@@ -5,7 +5,8 @@
 var request = require('request'),
     jsdom = require('jsdom');
 
-var scanLink = function(link) {
+var scanLink = function(links, index) {
+    link = 'http://www.vimgolf.com' + links[index++];
     request({uri: link}, function(err, response, body){
         if(err && response.statusCode !== 200){console.log('Request error.');}
 
@@ -14,40 +15,59 @@ var scanLink = function(link) {
             scripts: ['http://code.jquery.com/jquery-2.1.3.min.js'],
             done: function(err, window){
                 var $ = window.jQuery,
-                    user = $('.notice')
+                    users = window.$('.notice'),
                     firstUser = $('.notice > div'),
-                    lastUser = $('.notice:last-child > div');
+                    lastUser = $('.notice:last-child > div'),
+                    meUser = null,
+                    bestScore,
+                    worstScore,
+                    myScore;
                 
-                console.log(firstUser.eq(0).text());
-                console.log(lastUser.eq(0).text());
+                users.each(function() {
+                    var name = $(this).find('h6').text();
 
+                    if(name.match(/Oliver Christ/)) {
+                        meUser = $(this);
+                    }
+                });
 
+                bestScore = parseInt(firstUser.eq(0).text());
+                worstScore = parseInt(lastUser.text());
+
+                console.log(link);
+                if(meUser) {
+                    myScore = parseInt(meUser.find('> div').text());
+                    console.log('behind best: ' + (myScore - bestScore));
+                } else {
+                    console.log('not challenged');
+                }
+
+                if(index < links.length) {
+                    scanLink(links, index);
+                }
             }
         });
     });
 };
 
-//Tell the request that we want to fetch youtube.com, send the results to a callback function
-//request({uri: 'http://vimgolf.com'}, function(err, response, body){
-//    var self = this;
-//    self.items = new Array();//I feel like I want to save my results in an array
-//     
-//    if(err && response.statusCode !== 200){console.log('Request error.');}
-//
-//    jsdom.env({
-//        html: body,
-//        scripts: ['http://code.jquery.com/jquery-2.1.3.min.js'],
-//        done: function(err, window){
-//            var $ = window.jQuery,
-//                links = $('.challenge > a');
-//            
-//            links.each(function() {
-//                console.log($(this).text());
-//            });
-//        }
-//    });
-//
-//    scanLink('http://vimgolf.com/challenges/4e7dedb4f447090001000002');
-//});
+request({uri: 'http://vimgolf.com'}, function(err, response, body){
+    var self = this;
+    self.items = new Array();//I feel like I want to save my results in an array
+     
+    if(err && response.statusCode !== 200){console.log('Request error.');}
 
-scanLink('http://vimgolf.com/challenges/5462e3f41198b80002512673');
+    jsdom.env({
+        html: body,
+        scripts: ['http://code.jquery.com/jquery-2.1.3.min.js'],
+        done: function(err, window){
+            var $ = window.jQuery,
+                links = $('.challenge > a'),
+                linksHref = [];
+            
+            links.each(function() {
+                linksHref.push($(this).attr('href'));
+            });
+            scanLink(linksHref, 0);
+        }
+    });
+});
